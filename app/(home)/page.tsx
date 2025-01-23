cfee24e2f521b250d7b5dcab953d2994c81e06ae
Adem Can <ademcan.cert@gmail.com>
2025-01-23 22:53:49 +0300

    fix

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         </p>
       </div>
         <div className="space-y-8">
-        <p>Currently studying in the Graphic Design department, my true passion and expertise lie in frontend development. Although graphic design wasn’t my initial choice, it has provided me with valuable insights into visual design and user interfaces. Over time, I self-taught frontend development and started creating user-centered web applications that combine aesthetics and functionality.</p>
+        <p>Currently studying in the Graphic Design department, my true passion and expertise lie in frontend development. Although graphic design wasn&apos;t my initial choice, it has provided me with valuable insights into visual design and user interfaces. Over time, I self-taught frontend development and started creating user-centered web applications that combine aesthetics and functionality.</p>
         <hr className="border-0 border-b border-zinc-300 opacity-40" />
         <p>I'm passionate about creating minimalist and functional designs. By combining <strong>UI/UX</strong> design with frontend development, I focus on building intuitive interfaces that prioritize user experience, making them both visually appealing and effective.</p>
         <p>Outside of web development, I love playing story-driven games. Writing reviews after finishing a game has become a hobby.</p>
