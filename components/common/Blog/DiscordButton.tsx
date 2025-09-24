a69bf92bff1b576b2b4f3b680902fdb34ae3b9e2
ademcertell <ademcan.cert@gmail.com>
2025-09-24 20:09:07 +0300

    fixed

--- components/common/Blog/DiscordButton.tsx
+++ components/common/Blog/DiscordButton.tsx
@@ -3,7 +3,7 @@
 export default function DiscordButton({ title }: { title: string }) {
   return (
     <a
-      href={`https://discord.gg/vHZVGRAm3P`}
+      href={`https://discord.com/invite/MbUfMv9RhS`}
       target="_blank"
       rel="noopener noreferrer"
       className="mt-10 inline-flex items-center gap-2 rounded-xl border border-white/10 bg-white/5 px-4 py-2 text-sm hover:bg-white/10 transition-colors backdrop-blur"
